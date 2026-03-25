56691687e3152d2aa923e4296e6cc6f70514c703
hippy <hippy2981@gmail.com>
2026-03-25 08:14:32 -0700

    paste/vlip: Add vlip exe

---
+++ store/exe/paste.cs
@@ -0,0 +1,20 @@
+﻿// https://web.archive.org/web/20131124234800/http://huddledmasses.org/clipexe-and-the-missing-pasteexe
+// %WINDIR%\Microsoft.NET\Framework\v2.0.50727\csc.exe /out:vlip.exe paste.cs
+// %WINDIR%\Microsoft.NET\Framework\v4.0.30319\csc.exe /out:vlip.exe paste.cs
+
+using System;
+using System.Threading;
+using System.Windows.Forms;
+
+public class Program
+{
+	[STAThread]
+	static void Main(string[] args)
+	{
+		string[] lines = Clipboard.GetText().Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
+		foreach (string line in lines)
+		{
+			Console.WriteLine(line);
+		}
+	}
+}
